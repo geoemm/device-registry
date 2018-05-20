a020daf8800aca3a7650442eb36357815099020a
Georgios <geo.emm@hotmail.com>
2018-05-20 20:11:58 +0300

    Checking if the DB will clear

--- src/main/java/com/gemmano/dr/controllers/RegisterController.java
+++ src/main/java/com/gemmano/dr/controllers/RegisterController.java
@@ -1,9 +1,11 @@
 package com.gemmano.dr.controllers;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.gemmano.dr.services.RegisterService;
@@ -20,10 +22,11 @@ public class RegisterController {
 	}
 
 	@RequestMapping(method = RequestMethod.GET)
+    @ResponseStatus(HttpStatus.OK)
 	public String registerDevice(@RequestParam("id") long id) {//TODO check for min/max
 		
 		registerService.setDeviceState(id);
 		
-		return "SAVED";
+		return "OK";
 	}
 }
